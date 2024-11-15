693dc1d001012adb0b657c7883c05cf1aced35ac
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-11-14 16:15:24 -0800

    layout add footer

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,15 @@
-import { BODY, HTML, LINK, META } from "@fartlabs/htx";
+import {
+  A,
+  BODY,
+  BR,
+  FOOTER,
+  HR,
+  HTML,
+  LINK,
+  META,
+  P,
+  SMALL,
+} from "@fartlabs/htx";
 
 export function Layout(props: { children?: string[] }) {
   return (
@@ -7,6 +18,35 @@ export function Layout(props: { children?: string[] }) {
       <LINK rel="stylesheet" href="https://css.fart.tools/fart.css" />
       <BODY>
         {(props.children ?? []).join("")}
+
+        <FOOTER class="fart-section">
+          <HR />
+          <P>
+            © FartLabs <A class="fart-logo" href="https://fartlabs.org">🧪</A>
+            <BR />
+            <A
+              class="fart-button"
+              href="https://fartlabs.org/blog"
+              target="_blank"
+            >
+              Blog<SMALL>↗</SMALL>
+            </A>
+            <A
+              class="fart-button"
+              href="https://github.com/FartLabs"
+              target="_blank"
+            >
+              GitHub<SMALL>↗</SMALL>
+            </A>
+            <A
+              class="fart-button"
+              href="https://go.fart.tools/chat"
+              target="_blank"
+            >
+              Chat<SMALL>↗</SMALL>
+            </A>
+          </P>
+        </FOOTER>
       </BODY>
     </HTML>
   );
