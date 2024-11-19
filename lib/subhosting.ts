cd16a416b9a3ca0ff7c463508705c95e1e65f4cc
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-11-19 12:04:32 -0800

    wip add new profiles api handlers

--- lib/subhosting.ts
+++ lib/subhosting.ts
@@ -1,19 +1,5 @@
 import Subhosting from "subhosting";
 
-const subhosting = new Subhosting({
+export const subhosting = new Subhosting({
   bearerToken: Deno.env.get("DEPLOY_ACCESS_TOKEN"),
 });
-
-// deno run -A lib/subhosting.ts
-if (import.meta.main) {
-  const projects = await subhosting.organizations.projects.list(
-    Deno.env.get("DEPLOY_ORG_ID")!,
-  );
-  console.log({ projects });
-
-  const project = await subhosting.organizations.projects.create(
-    Deno.env.get("DEPLOY_ORG_ID")!,
-    { description: "[TEST]" },
-  );
-  console.log({ project });
-}
---
+++ routes/api/profiles-logs-api-handler.ts
@@ -0,0 +1,33 @@
+import type { Handler } from "@std/http";
+import type { Helpers } from "@deno/kv-oauth";
+import { getUserBySessionID } from "#/lib/kv/get-user-by-session-id.ts";
+
+/**
+ * makeProfilesLogsAPIHandler makes an endpoint for reading the logs of a profile's
+ * deployments.
+ */
+export function makeProfilesLogsAPIHandler(
+  kv: Deno.Kv,
+  { getSessionId }: Helpers,
+): Handler {
+  return async function handler(request) {
+    // Check if user is signed in. Else, redirect to /signin.
+    const sessionID = await getSessionId(request);
+    if (sessionID === undefined) {
+      return new Response(null, {
+        status: 303,
+        headers: new Headers({ Location: "/signin" }),
+      });
+    }
+
+    // Get the user from the session ID.
+    const user = await getUserBySessionID(kv, sessionID);
+    if (user.value === null) {
+      return new Response("Internal server error", { status: 500 });
+    }
+
+    // TODO: Check if profile ID passed in URL pathname is owned by user.
+
+    throw new Error("Not implemented");
+  };
+}
